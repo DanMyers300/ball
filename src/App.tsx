8e734e1e58bc6030426601bd6fef65e67ad753e8
dan <contact@danmyers.net>
2025-05-02 19:20:21 -0500

    background black

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 const App = () => {
   return (
     <>
+      <main className="
+        w-screen
+        h-screen
+        bg-black
+      ">
+        <div className="
+          bg-red
+          w-50
+          h-50
+        ">
+        </div>
+      </main>
     </>
   )
 }
