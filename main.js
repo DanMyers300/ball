54d55308ca4bde57740847241e2981eebf5bc958
dan <contact@danmyers.net>
2025-05-30 13:30:47 -0500

    added x

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ let xPosition = 0;
 let yVelocity = 0;
 let xVelocity = 0;
 let isDragging = false;
+const xDrag = 0.7;
+const yDrag = 0.99;
 const size = 70;
-const gravity = 0.5
+const gravity = 0.5;
+let mouseStartX = 0;
+let mouseStartY = 0;
+let bounceFactor = 0.9;
 const containerHeight = window.innerHeight - size;
 const containerWidth = window.innerWidth - size;
 
@@ -15,26 +20,34 @@ ball.style.cursor = 'move';
 
 const handleMouseUp = () => {
     isDragging = false;
-    requestAnimationFrame(() => 
-        animate()
-    )
+    requestAnimationFrame(() => animate());
 };
 
 const handleMouseMove = (e) => {
-    if (!isDragging) return
+    if (!isDragging) return;
 
     const top = e.clientY - size;
-    ball.style.top = `${top}px`
+    ball.style.top = `${top}px`;
     yPosition = top;
 
     const left = e.clientX - size;
-    ball.style.left = `${left}px`
+    ball.style.left = `${left}px`;
+    xPosition = left;
+
+    xVelocity = e.clientX - mouseStartX;
+    mouseStartX = e.clientX;
+
+    yVelocity = e.clientY - mouseStartY;
+    mouseStartY = e.clientY;
 };
 
-const handleMouseDown = () => {
+const handleMouseDown = (e) => {
     isDragging = true;
     yPosition = parseInt(ball.style.top);
     xPosition = parseInt(ball.style.left);
+    yVelocity = 0;
+    mouseStartX = e.clientX;
+    mouseStartY = e.clientY;
 };
 
 const handleMouse = () => {
@@ -44,27 +57,43 @@ const handleMouse = () => {
 };
 
 function animate() {
-
     if (isDragging) return;
 
-    if (Math.abs(yVelocity) < 0.05) {
+    if (Math.abs(yVelocity) < 0.1) {
         yVelocity = 0;
     }
 
-    if (yPosition < containerHeight) {
-        yVelocity *= 0.995
+    if (Math.abs(xVelocity) < 0.1) {
+        xVelocity = 0;
+    }
+
+    if (yPosition < 0) {
+        yVelocity *= yDrag;
+        yVelocity = -yVelocity;
+        yPosition += yVelocity;
+    } else if (yPosition < containerHeight) {
+        yVelocity *= yDrag;
         yVelocity += gravity;
         yPosition += yVelocity;
-    } else if (
-        yPosition >= containerHeight || yPosition <= 0
-    ) {
-        yVelocity *= 0.995
-        yVelocity = -yVelocity
+    } else if (yPosition >= containerHeight) {
+        yVelocity *= yDrag;
+        yVelocity = -yVelocity;
         yPosition += yVelocity;
     }
 
+    xPosition += xVelocity;
+    if (xPosition < 0) {
+        xVelocity *= xDrag;
+        xVelocity = -xVelocity;
+        xPosition += xVelocity;
+    } else if (xPosition > containerWidth) {
+        xVelocity *= xDrag;
+        xVelocity = -xVelocity;
+        xPosition += xVelocity;
+    }
+
     ball.style.top = `${yPosition}px`;
-    //ball.style.left = `${xPosition}px`;
+    ball.style.left = `${xPosition}px`;
     requestAnimationFrame(animate);
 }
 
